1bf77fb88959dd7b0838f23f6abfac0c0aea5a8c
Ubada23 <ubadaaleem14@gmai.com>
2026-04-26 18:47:06 +0500

    Add custom QuestionAdmin class

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-
 from .models import Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
 
-
-# Register your models here.
+admin.site.register(Question, QuestionAdmin)
