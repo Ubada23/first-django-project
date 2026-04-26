016cb6ea7670aed22c9f764d8163468f8b99cc86
Ubada23 <ubadaaleem14@gmai.com>
2026-04-26 20:48:01 +0500

    Display actual question text in detail view

--- polls/views.py
+++ polls/views.py
@@ -10,7 +10,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = Question.objects.get(pk=question_id)
+    return HttpResponse(question.question_text)
 
 
 def results(request, question_id):
